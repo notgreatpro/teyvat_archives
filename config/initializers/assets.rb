88698f4d598e568dc0e7be6008314021fbff18a6
itsgreatpro <karahan@academic.rrc.ca>
2025-11-08 19:01:52 -0600

    edited some html.erb as well as edited css ensuring its loaded in the browser

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,3 +5,8 @@ Rails.application.config.assets.version = "1.0"
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+# Add font paths
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+# Precompile fonts
+Rails.application.config.assets.precompile += %w( *.ttf *.woff *.woff2 *.eot *.svg )
